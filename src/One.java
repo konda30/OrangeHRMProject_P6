2938832d1517a054d93d6cacbdedfdfc0f4efc46
digu <digu@namrata>
2023-05-03 11:52:43 +0530

    first commit

---
+++ src/One.java
@@ -0,0 +1,6 @@
+
+public class One {
+	public static void main(String arg[]) {
+		System.out.println("hi");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author digu
+ *
+ */
+module gitproject {
+}
